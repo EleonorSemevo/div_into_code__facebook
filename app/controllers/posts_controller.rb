6d367c53972b5389d4acac22d7dc6af7b6051b47
EleonorSemevo <eleonorsemevo@gmail.com>
2021-08-22 23:40:26 +0100

    quelques insuffusance corrigées

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,35 +1,25 @@
 class PostsController < ApplicationController
   before_action :set_post, only: %i[ show edit update destroy ]
-
-  # GET /posts or /posts.json
   def index
     @posts = Post.all
   end
 
-  # GET /posts/1 or /posts/1.json
   def show
   end
 
-  # GET /posts/new
   def new
     @post = Post.new
   end
 
   def confirm
-    # @post =Post.new(post_params)
-    # @post.user_id = current_user.id
     @post = current_user.posts.build(post_params)
     render :new if @post.invalid?
   end
 
-  # GET /posts/1/edit
   def edit
   end
 
-  # POST /posts or /posts.json
   def create
-    # @post = Post.new(post_params)
-    # @post.user = current_user
     @post = current_user.posts.build(post_params)
 
     respond_to do |format|
@@ -43,7 +33,6 @@ class PostsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /posts/1 or /posts/1.json
   def update
     respond_to do |format|
       if @post.update(post_params)
@@ -66,13 +55,11 @@ class PostsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_post
-      @post = Post.find(params[:id])
-    end
+  def set_post
+    @post = Post.find(params[:id])
+  end
 
-    # Only allow a list of trusted parameters through.
-    def post_params
-      params.require(:post).permit(:content, :image, :image_cache)
-    end
+  def post_params
+    params.require(:post).permit(:content, :image, :image_cache)
+  end
 end
