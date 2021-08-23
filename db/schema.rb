2dfb36d4fa6a8db0f436db0f922a38d8463eec89
EleonorSemevo <eleonorsemevo@gmail.com>
2021-08-23 10:47:37 +0100

    Des erreurs corrigés

--- db/schema.rb
+++ db/schema.rb
@@ -12,10 +12,13 @@
 
 ActiveRecord::Schema.define(version: 2021_08_19_085248) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "posts", force: :cascade do |t|
     t.text "content"
     t.string "image"
-    t.integer "user_id"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
@@ -30,4 +33,5 @@ ActiveRecord::Schema.define(version: 2021_08_19_085248) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
