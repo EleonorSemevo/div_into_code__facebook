69b11b4dd25484d6c4e7087e0a27eb2b1d0148cf
EleonorSemevo <eleonorsemevo@gmail.com>
2021-08-25 16:16:25 +0100

    configuration de mailer pour production environnement

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,19 +4,7 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
   config.action_mailer.default_url_options = { host: 'https://peaceful-atoll-18348.herokuapp.com' }
-  config.delivery_method = :smtp
-  config.perform_deliveries = true
-  config.raise_delivery_errors = true
-  config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               'gmail.com',
-    user_name:            'eleonorsemevo@gmail.com',
-    password:             '@misericorde123#',
-    authentication:       'plain'
-    # enable_starttls_auto: true
-    # ^ ^ remove this option ^ ^
-  }
+
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
