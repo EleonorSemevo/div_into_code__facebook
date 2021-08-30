21c72e3f3fb516d35c1b24b2e949838c4f6f460d
EleonorSemevo <eleonorsemevo@gmail.com>
2021-08-30 15:11:28 +0100

    la validation est ajouté au model de post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
+  validates :content, presence: true, length: {minimum: 2}
+  validates :image, presence: true
   belongs_to :user
   mount_uploader :image, ImageUploader
   has_many :favorites, dependent: :destroy
